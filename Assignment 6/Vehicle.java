497e58fc1fb9656d122367856abe30ab0b5dd68c
Arinjay <92075314+Jackboes@users.noreply.github.com>
2026-08-09 18:41:44 +0530

    Update Vehicle.java

--- Assignment 6/Vehicle.java
+++ Assignment 6/Vehicle.java
@@ -1,1 +1,35 @@
+class Vehicle {
+    String name;
+    int speed;
 
+    Vehicle(String name, int speed) {
+        this.name = name;
+        this.speed = speed;
+    }
+
+    // Inner class
+    class VehicleDetails {
+        void displayDetails() {
+            System.out.println("Vehicle: " + name);
+            System.out.println("Speed: " + speed + " km/h");
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Vehicle v = new Vehicle("BMW", 200);
+
+        // Creating object of inner class
+        VehicleDetails details = v.new VehicleDetails();
+        details.displayDetails();
+
+        // Anonymous class
+        Runnable action = new Runnable() {
+            public void run() {
+                System.out.println("Vehicle is starting...");
+            }
+        };
+
+        action.run();
+    }
+}
